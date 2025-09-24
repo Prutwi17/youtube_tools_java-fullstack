a6104d6711c460568d192461335ee0ceb21948ca
Prutwi17 <pruthwiraj314@gmail.com>
2025-09-25 03:57:41 +0530

    Initial commit

---
+++ Java-Full-Stack-Project-main/src/main/java/com/YouTubeTools/Service/YouTubeService.java
@@ -0,0 +1,15 @@
+package com.YouTubeTools.Service;
+
+
+import com.YouTubeTools.Model.SearchVideo;
+import org.springframework.stereotype.Service;
+
+import java.lang.classfile.instruction.ReturnInstruction;
+
+@Service
+public class YouTubeService {
+
+    public SearchVideo searchVideos(String videoTitle) {
+        return ;
+    }
+}
